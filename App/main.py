284b2dd6422d8f0c2a45b178ede87ff57c7539f3
Michelle <michelle.01.liu@outlook.com>
2022-04-21 02:02:42 +0000

    Place app.run() behind guard

--- App/main.py
+++ App/main.py
@@ -140,4 +140,5 @@ def signupAction():
 def dashboard():
     return render_template('dashboard.html')
 
-app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
