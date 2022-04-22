977951511e81d8be733afdda65bd4bb32ccb9e70
Michelle <michelle.01.liu@outlook.com>
2022-04-22 04:42:29 +0000

    Add AlumnusInfo form

--- App/forms.py
+++ App/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SubmitField, RadioField
-from wtforms.validators import InputRequired, EqualTo, Email
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SubmitField, RadioField, DateField, IntegerField, SelectField
+from wtforms.validators import InputRequired, NumberRange, EqualTo, Email
 
 class SignUp(FlaskForm):
     firstName = StringField('First Name', validators=[InputRequired()])
@@ -15,3 +15,10 @@ class LogIn(FlaskForm):
     username = StringField('Enter your username', validators=[InputRequired()])
     password = PasswordField('Enter your password', validators=[InputRequired()])
     submit = SubmitField('Login', render_kw={'type' : 'submit', 'class' : 'col s12 btn btn-large waves-effect waves-light indigo'})
+
+class AlumnusInfo(FlaskForm):
+    gradYear = IntegerField('Graduation Year', validators=[NumberRange(min=0, max=4)])
+    faculty = StringField('Faculty')
+    department = StringField('Department')
+    programme = StringField('Programme')
+    save = SubmitField('Save', render_kw={'type' : 'submit', 'class' : 'col s12 btn btn-large waves-effect waves-light indigo'})
