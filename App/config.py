507098b5d0c81071e9fa1a0828b417b768c44335
Michelle <michelle.01.liu@outlook.com>
2022-04-24 01:38:01 +0000

    Change postgres to postgresql

--- App/config.py
+++ App/config.py
@@ -1,6 +1,7 @@
-DBURI = "postgres://iyvpyncuikxtmm:748440c014d5f7ef36a34bdb6c760242e8a47224975631a0c07edcb9ee057710@ec2-54-80-122-11.compute-1.amazonaws.com:5432/dbt01ad2m2e482"
+DBURI = "postgresql://iyvpyncuikxtmm:748440c014d5f7ef36a34bdb6c760242e8a47224975631a0c07edcb9ee057710@ec2-54-80-122-11.compute-1.amazonaws.com:5432/dbt01ad2m2e482"
+SQLITEDB = False
 # SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+# SQLITEDB = True
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "DEVELOPMENT"
-SQLITEDB = False
